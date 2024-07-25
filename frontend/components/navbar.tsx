e5e477fb977b62abaf50dd5cef6170218000c3cb
Maciek99 <64065294+Maciek99@users.noreply.github.com>
2024-07-25 16:42:04 +0200

    Update navbar styling

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { FaMoon, FaSun, FaUser, FaSignOutAlt } from "react-icons/fa";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
 
 interface NavbarProps {
   children?: React.ReactNode;
@@ -11,7 +13,7 @@ interface NavbarProps {
 
 const Navbar: React.FC<NavbarProps> = ({ children }) => {
   const pathname = usePathname();
-  const [theme, setTheme] = React.useState<"light" | "dark">("light");
+  const {theme, setTheme} = useTheme ();
 
   const toggleTheme = () => {
     setTheme(theme === "light" ? "dark" : "light");
@@ -19,14 +21,15 @@ const Navbar: React.FC<NavbarProps> = ({ children }) => {
   };
 
   return (
-    <nav className="bg-white shadow-md border-b-2 border-red-600">
+    <nav className="bg-card shadow-md border-b-2 border-red-600">
       <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex items-center">{children}</div>
           <div className="flex items-center space-x-4">
-            <button
+            <Button
               onClick={toggleTheme}
-              className="p-2 rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className="p-2 aspect-square"
+              variant="outline"
               aria-label={
                 theme === "light"
                   ? "Switch to dark mode"
@@ -34,17 +37,17 @@ const Navbar: React.FC<NavbarProps> = ({ children }) => {
               }
             >
               {theme === "light" ? <FaMoon /> : <FaSun />}
-            </button>
+            </Button>
             <Link
               href="/profile"
-              className="text-gray-600 hover:text-gray-800"
+              className="text-secondary-foreground hover:text-foreground"
               aria-label="Profile"
             >
               <FaUser />
             </Link>
             <Link
               href="/logout"
-              className="text-gray-600 hover:text-gray-800"
+              className="text-secondary-foreground hover:text-foreground"
               aria-label="Logout"
             >
               <FaSignOutAlt />
