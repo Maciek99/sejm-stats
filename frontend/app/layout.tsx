55d60691318151c6692856382d3d6873a2c90506
Maciek99 <64065294+Maciek99@users.noreply.github.com>
2024-07-25 17:01:51 +0200

    refactor: Update icon colors in layout component

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -89,12 +89,12 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     {
       href: "https://discord.com/invite/zH2J3z5Wbf",
       label: "Discord",
-      icon: <FaDiscord className="w-5 h-5 text-blue-600" />,
+      icon: <FaDiscord className="w-5 h-5 text-[#5865f2]" />,
     },
     {
       href: "https://github.com/miskibin/sejm-stats",
       label: "Github",
-      icon: <FaGithub className="w-5 h-5 text-gray-600" />,
+      icon: <FaGithub className="w-5 h-5 text-gray-600 dark:text-white" />,
     },
   ];
 
