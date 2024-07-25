4eede57c2d42fda02bf7593b22520cab628c85e5
Maciek99 <64065294+Maciek99@users.noreply.github.com>
2024-07-25 17:26:18 +0200

    refactor: Improve lazy loading for clubs page components

--- frontend/app/clubs/page.tsx
+++ frontend/app/clubs/page.tsx
@@ -1,7 +1,7 @@
 import ClubsList from "@/components/clubList";
 import ClubsChart from "@/components/clubChart";
 import { fetchAllActs, fetchAllClubs } from "@/lib/api";
-
+import LoadableContainer from "@/components/loadableContainer"
 export default async function ClubsPage() {
   const clubsData = await fetchAllClubs();
 
@@ -9,18 +9,18 @@ export default async function ClubsPage() {
     <div className="container mx-auto mt-4 px-4">
       <hr className="my-4" />
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-        <div className="mx-auto bg-card  rounded shadow-md my-4 md:mx-3 2xl:mx-16 md:px-4 max-md:px-1 py-3">
+        <LoadableContainer>
           <h2 className="text-2xl font-normal text-secondary-foreground  mb-4">
             Lista Klubów
           </h2>
           <ClubsList clubs={clubsData} />
-        </div>
-        <div className="mx-auto bg-card  rounded shadow-md my-4 md:mx-3 2xl:mx-16 md:px-4 max-md:px-1 py-3">
+        </LoadableContainer>
+        <LoadableContainer>
           <h2 className="text-2xl font-normal text-secondary-foreground mb-4">
             Rozkład mandatów{" "}
           </h2>
           <ClubsChart clubs={clubsData} />
-        </div>
+        </LoadableContainer>
       </div>
     </div>
   );
